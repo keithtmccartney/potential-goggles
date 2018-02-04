ad527672c8eedafff3f714672bd3e455ac8a3a17
Keith <keithtmccartney@hotmail.com>
2018-02-04 15:14:40 +0000

    Adding 'charts' class.

---
+++ scripts/charts.js
@@ -0,0 +1,22 @@
+var pointLimit = 15; //Total amount of points on your graph.
+
+var topPadding = 100; // Additional room between the top most point and the top of the chart
+
+var bottomPadding = 100; // Additional room for the bottom of the graph
+
+var eonData = {
+    labels: true, type: 'line'
+}; // flag for rendering labels with the point and the type of line that will be rendered
+
+var eonAxis = {
+    y: {
+        padding: {
+            top: topPadding, bottom: bottomPadding
+        }
+    },
+    x: {
+        type: 'timeseries', tick: {
+            format: '%H:%M:%S'
+        }
+    }
+}; // Formatting the time stamps along the bottom of the graphs.
